c8cc317dddf8d8d6609d6076dbdccd1d35e176fa
平野流 <ruihirano@RuiHirano-MacBook-Pro.local>
2025-10-09 23:28:18 +0900

    apply pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      devOptions: { enabled: true },
+      manifest: {
+        name: "Miel",
+        icons: [
+          {
+            sizes: "192x192",
+            src: "miel-logo-transparent-192.png",
+            type: "image/png",
+          },
+          {
+            sizes: "512x512",
+            src: "miel-logo-transparent-512.png",
+            type: "image/png",
+          },
+          {
+            sizes: "512x512",
+            src: "miel-logo-transparent-512.png",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
